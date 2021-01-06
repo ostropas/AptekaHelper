3a75396d1602fd3e9c8f71eb421cc3707ceabb16
Pavel Ostroukhov <p.ostroukhov@doudougames.ru>
2021-01-06 18:39:44 +0300

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DesctopAptekaHelper
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
